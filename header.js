34cdf4771f0221c39a451032d9c5b5d86478168d
currently_a_noob <161359799+studentofcodes@users.noreply.github.com>
2025-01-26 21:52:33 +0800

    Create header.js

---
+++ header.js
@@ -0,0 +1,13 @@
+// Script to manage the header content
+document.getElementById('header').innerHTML = `
+  <header>
+    <h1>Welcome to My Website</h1>
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+`;
